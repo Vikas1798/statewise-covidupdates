25d4c8eb2fb697ae16599bfe604fa8e5f1e8d495
Vikas1798 <76646685+Vikas1798@users.noreply.github.com>
2021-06-14 13:09:15 +0530

    my covid state wise update app

---
+++ src/components/Statewise.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import "./Statewise.css";
+
+function numFormatter(num) {
+  if (num > 999 && num < 1000000) {
+    return (num / 1000).toFixed(1) + "K"; // convert to K for number from > 1000 < 1 million
+  } else if (num > 1000000) {
+    return (num / 1000000).toFixed(1) + "M"; // convert to M for number from > 1 million
+  } else if (num < 900) {
+    return num; // if value < 1000, nothing to do
+  }
+}
+const Statewise = () => {
+  const [data, setData] = useState([]);
+
+  const getCovidData = async () => {
+    const res = await fetch("https://api.covid19india.org/data.json");
+    const actualData = await res.json();
+    setData(actualData.statewise);
+  };
+
+  useEffect(() => {
+    getCovidData();
+  }, []);
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="main-heading">
+          <div className="text-center text-white">
+            COVID &nbsp;
+            <span>
+              <img
+                src="https://st.depositphotos.com/1820591/1447/v/600/depositphotos_14478983-stock-illustration-creative-indian-flag-design.jpg"
+                alt="india logo"
+                id="logo"
+                width="200px"
+                className="flagimg"
+              />
+            </span>
+            Dashboard
+          </div>
+        </div>
+        <div className="table-responsive">
+          <table className="table table-hover">
+            <thead className="table-dark text-white">
+              <tr>
+                <th> State</th>
+                <th> Confirmed</th>
+                <th> Recovered</th>
+                <th> Deaths</th>
+                <th> Active</th>
+                <th> Updated</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((curEle, index) => {
+                return (
+                  <>
+                    <tr key={index}>
+                      <td className="state-css"> {curEle.state}</td>
+                      <td className="confirmed-css text-center">
+                        {numFormatter(curEle.confirmed)}
+                        <span className="arrows">
+                          <i
+                            className="fas fa-arrow-up"
+                            style={{ color: "red" }}
+                          ></i>
+                        </span>
+                      </td>
+                      <td className="recovered-css text-center">
+                        {numFormatter(curEle.recovered)}
+                        <span className="arrows">
+                          <i
+                            className="fas fa-arrow-down"
+                            style={{ color: "rgb(0, 255, 64)" }}
+                          ></i>
+                        </span>
+                      </td>
+                      <td className="deaths-css text-center">
+                        {" "}
+                        {numFormatter(curEle.deaths)}
+                      </td>
+                      <td className="active-css text-center">
+                        {" "}
+                        {numFormatter(curEle.active)}
+                      </td>
+                      <td> {curEle.lastupdatedtime}</td>
+                    </tr>
+                  </>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <footer className="footer text-center text-white">
+        <p> ---Developed by @Vikashk--- </p>
+      </footer>
+    </>
+  );
+};
+
+export default Statewise;
